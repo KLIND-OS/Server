8dade1e2552907615ad0c981de8fe67fbf986714
JZITNIK-github <email@jzitnik.is-a.dev>
2023-10-26 09:11:37 +0200

    minor changes and eslint

--- inputDevices.js
+++ inputDevices.js
@@ -1,22 +1,22 @@
 class InputDevices {
     static init(win) {
-        const select = win.querySelector(".select")
+        const select = win.querySelector(".select");
         LowLevelApi.InputSound.getAll(response => {
             LowLevelApi.InputSound.getDefault(resDefault => {
                 for (const inputDevice of response) {
-                    const choice = document.createElement("option")
-                    choice.value = inputDevice
-                    choice.textContent = inputDevice
+                    const choice = document.createElement("option");
+                    choice.value = inputDevice;
+                    choice.textContent = inputDevice;
                     if (inputDevice == resDefault) {
-                        choice.selected = true
+                        choice.selected = true;
                     }
-                    select.appendChild(choice)
+                    select.appendChild(choice);
                 }
-            })
-        })
+            });
+        });
         select.addEventListener("change", (e) => {
-            const {value} = e.target
-            LowLevelApi.InputSound.set(value)
-        })
+            const {value} = e.target;
+            LowLevelApi.InputSound.set(value);
+        });
     }
 }
