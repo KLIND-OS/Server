567deb6fab2e5bfd913ed26f65674e657f0ddaf0
JZITNIK-github <email@jzitnik.is-a.dev>
2023-12-04 16:49:52 +0100

    i have no fucking idea what i am doing with my life.

--- loadingerror.js
+++ loadingerror.js
@@ -11,3 +11,10 @@ setTimeout(() => {
   }
 
 }, 30 * 1000);
+/*
+  Nic mi nedává smysl: žít, programovat, učit se.
+  Nemám rád svůj život. Nevím jestli chci vůbec programovat, ale je to jediné co umít.
+  Nerad říkám že mám deprese, ale lidi mi to někdy říkají. Druzí říkají:
+  "Je to jenom obdobím. Měl bys jít na vojnu aby si neměl takové myšlenky.
+  Na vojně jsme neměli čas nad tím přemýšlet". Fakt díky, to mi pomohlo.
+*/
