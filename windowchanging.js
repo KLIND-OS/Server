62c70a1578c69f4e1dd47c10a086a01f28ff68f8
JZITNIK-github <email@jzitnik.is-a.dev>
2023-12-27 11:10:44 +0100

    Make shortcuts on windowchange window

--- windowchanging.js
+++ windowchanging.js
@@ -1,6 +1,5 @@
 window.addEventListener("click", function (e) {
   if (document.querySelector(".openopenedapps").contains(e.target)) {
-    // Clicked in box
   } else {
     if (appsopened.oppened) {
       appsopened.close();
@@ -12,6 +11,7 @@ var appsopened = {
   count: 0,
   list: [],
   oppened: false,
+  selected: undefined,
   openwin: (value) => {
     var element = document.querySelectorAll(".window.openedwin")[value];
     changewindowmain(element);
@@ -22,17 +22,57 @@ var appsopened = {
     var element = document.querySelector(".openopenedapps");
     if (appsopened.count != 0) {
       var innerHTMLvalue = "";
-      for (var i = 0; i < appsopened.count; i++) {
-        if (appsopened.list[i][2] == openedwindowindex) {
-          innerHTMLvalue += "<div class=\"selected buttonopenedapps " + appsopened.list[i][0] + "vybranydiv\" onclick=\"appsopened.openwin(" + i + ")\">" + appsopened.list[i][1] + "</div>";
+      if (appsopened.selected !== undefined) {
+        for (var i = 0; i < appsopened.count; i++) {
+          if (i == appsopened.selected) {
+            innerHTMLvalue +=
+              '<div class="selected buttonopenedapps ' +
+              appsopened.list[i][0] +
+              'vybranydiv" onclick="appsopened.openwin(' +
+              i +
+              ')">' +
+              appsopened.list[i][1] +
+              "</div>";
+          } else {
+            innerHTMLvalue +=
+              '<div class="buttonopenedapps ' +
+              appsopened.list[i][0] +
+              'vybranydiv" onclick="appsopened.openwin(' +
+              i +
+              ')">' +
+              appsopened.list[i][1] +
+              "</div>";
+          }
         }
-        else {
-          innerHTMLvalue += "<div class=\"buttonopenedapps " + appsopened.list[i][0] + "vybranydiv\" onclick=\"appsopened.openwin(" + i + ")\">" + appsopened.list[i][1] + "</div>";
+      } else {
+        for (var i = 0; i < appsopened.count; i++) {
+          if (appsopened.list[i][2] == openedwindowindex) {
+            innerHTMLvalue +=
+              '<div class="selected buttonopenedapps ' +
+              appsopened.list[i][0] +
+              'vybranydiv" onclick="appsopened.openwin(' +
+              i +
+              ')">' +
+              appsopened.list[i][1] +
+              "</div>";
+            appsopened.selected = i;
+          } else {
+            innerHTMLvalue +=
+              '<div class="buttonopenedapps ' +
+              appsopened.list[i][0] +
+              'vybranydiv" onclick="appsopened.openwin(' +
+              i +
+              ')">' +
+              appsopened.list[i][1] +
+              "</div>";
+          }
+        }
+        if (appsopened.selected === undefined) {
+          appsopened.selected = 0;
         }
       }
       element.innerHTML = innerHTMLvalue;
-    }
-    else {
+    } else {
       element.innerHTML = "Nejsou otevřené žádné okna";
     }
     appsopened.oppened = true;
@@ -42,15 +82,45 @@ var appsopened = {
     appsopened.oppened = false;
     var element = document.querySelector(".openopenedapps");
     element.style.display = "none";
-  }
+    appsopened.selected = undefined;
+  },
 };
 setInterval(() => {
   appsopened.list = [];
   var elements = document.querySelectorAll(".window.openedwin");
   for (var i = 0; i < elements.length; i++) {
     try {
-      appsopened.list.push([elements[i].classList[1], elements[i].querySelector(".headerclass span").innerHTML, elements[i]]);
-    } catch { }
+      appsopened.list.push([
+        elements[i].classList[1],
+        elements[i].querySelector(".headerclass span").innerHTML,
+        elements[i],
+      ]);
+    } catch {}
   }
   appsopened.count = appsopened.list.length;
 }, 500);
+document.addEventListener("keypress", (e) => {
+  if (appsopened.oppened) {
+    if (e.key == "a") {
+      if (appsopened.selected !== undefined) {
+        if (appsopened.selected == 0) {
+          return;
+        }
+        appsopened.selected--;
+      }
+      appsopened.open();
+    } else if (e.key == "d") {
+      if (appsopened.selected !== undefined) {
+        if (appsopened.selected == appsopened.count - 1) {
+          return;
+        }
+        appsopened.selected++;
+      }
+      appsopened.open();
+    } else if (e.key == "Enter") {
+      appsopened.openwin(appsopened.selected);
+    } else if (e.key == "q") {
+      appsopened.close();
+    }
+  }
+});
