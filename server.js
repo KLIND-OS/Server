8ed47ae80e17ca8fe74cf57dc602cb814b05ce21
JZITNIK-github <email@jzitnik.is-a.dev>
2023-12-30 21:47:30 +0100

    Update development server

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var livereload = require("livereload");
+const chokidar = require('chokidar');
+var connectLiveReload = require("connect-livereload");
+const port = 10000
+
+const watcher = chokidar.watch(".", {
+  ignored: [".git"],
+  persistent: true,
+});
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+if (process.argv[2] == "dev") {
+  watcher
+    .on('add', (e) => {
+      liveReloadServer.refresh("/");
+    })
+    .on('change', (e) => {
+      liveReloadServer.refresh("/");
+    })
+    .on('unlink', (e) => {
+      liveReloadServer.refresh("/");
+    });
+}
+
+var app = express();
+
+app.use(connectLiveReload());
+
+function skipMiddleware(req, res) {
+  return req.url.includes('/status');
+}
+
+app.use(logger('dev', { skip: skipMiddleware }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, ".")));
+
+app.listen(port, '127.0.0.1', () => {
+  console.log(`KLIND OS Server is running on port ${port}`);
+});
--- server.py
+++
@@ -1,3 +0,0 @@
-import os
-print("Starting server")
-os.system("python -m http.server 10000")
