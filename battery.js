e1abd31339d165fa50b830cd4cda8b7206c22c62
JZITNIK-github <email@jzitnik.is-a.dev>
2024-03-10 10:06:25 +0100

    Many changes

--- battery.js
+++ battery.js
@@ -1,23 +1,21 @@
 function loadBattery() {
-  navigator.getBattery()
-    .then(battery => {
-      const level = Math.round(battery.level * 100);
-      document.querySelector("#batteryLevel").textContent = `${level}%`;
+  const { status, percentage, battery } = LowLevelApi.Battery.getBattery();
 
-      if (battery.charging) {
-        document.querySelector("#batteryIcon").style.display = "none";
-        document.querySelector("#batteryIconCharging").style.display = "block";
-      }
-      else {
-        document.querySelector("#batteryIcon").style.display = "block";
-        document.querySelector("#batteryIconCharging").style.display = "none";
-      }
-    })
-    .catch(err => {
-      error();
-    });
+  if (!battery) {
+    document.querySelector(".batteryStatus").style.display = "none";
+    return
+  }
+
+    document.querySelector(".batteryStatus").style.display = "block";
+  document.querySelector("#batteryLevel").textContent = percentage.trim();
+  if (status.trim() == "Discharging") {
+    document.querySelector("#batteryIcon").style.display = "block";
+    document.querySelector("#batteryIconCharging").style.display = "none";
+  } else {
+    document.querySelector("#batteryIcon").style.display = "none";
+    document.querySelector("#batteryIconCharging").style.display = "block";
+  }
 }
 
-setInterval(loadBattery, 2000);
+setInterval(loadBattery, 5000);
 loadBattery();
-
