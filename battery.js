6374bc4bdc6c2f553c395f6b54f5617aa0446887
JZITNIK-github <email@jzitnik.is-a.dev>
2024-03-15 18:02:37 +0100

    Add battery sounds

--- battery.js
+++ battery.js
@@ -1,19 +1,30 @@
+let previousBatteryState = "notcharging";
+
 async function loadBattery() {
-  const { status, percentage, battery } = await LowLevelApi.Battery.getBattery();
+  const { status, percentage, battery } =
+    await LowLevelApi.Battery.getBattery();
 
   if (!battery) {
     document.querySelector(".batteryStatus").style.display = "none";
-    return
+    return;
   }
 
-    document.querySelector(".batteryStatus").style.display = "block";
+  document.querySelector(".batteryStatus").style.display = "block";
   document.querySelector("#batteryLevel").textContent = percentage.trim();
   if (status.trim() == "Discharging") {
     document.querySelector("#batteryIcon").style.display = "block";
     document.querySelector("#batteryIconCharging").style.display = "none";
+    if (previousBatteryState !== "notcharging") {
+      batteryOut()
+    }
+    previousBatteryState = "notcharging";
   } else {
     document.querySelector("#batteryIcon").style.display = "none";
     document.querySelector("#batteryIconCharging").style.display = "block";
+    if (previousBatteryState == "charging") {
+      batteryIn()
+    }
+    previousBatteryState = "charging";
   }
 }
 
