ad9b6cc429acbf7a37f5ab0e56ba044ab76b33bd
JZITNIK-github <email@jzitnik.is-a.dev>
2024-03-17 18:02:56 +0100

    Update battery logic

--- battery.js
+++ battery.js
@@ -1,8 +1,7 @@
 let previousBatteryState = "notcharging";
 
 async function loadBattery() {
-  const { status, percentage, battery } =
-    await LowLevelApi.Battery.getBattery();
+  const { status, percentage, battery } = await LowLevelApi.Battery.getBattery();
 
   if (!battery) {
     document.querySelector(".batteryStatus").style.display = "none";
@@ -11,19 +10,16 @@ async function loadBattery() {
 
   document.querySelector(".batteryStatus").style.display = "block";
   document.querySelector("#batteryLevel").textContent = percentage.trim();
-  if (status.trim() == "Discharging") {
+
+  if (status.trim() === "Discharging" && previousBatteryState !== "notcharging") {
     document.querySelector("#batteryIcon").style.display = "block";
     document.querySelector("#batteryIconCharging").style.display = "none";
-    if (previousBatteryState !== "notcharging") {
-      batteryOut()
-    }
+    batteryOut();
     previousBatteryState = "notcharging";
-  } else {
+  } else if (status.trim() === "Charging" && previousBatteryState !== "charging") {
     document.querySelector("#batteryIcon").style.display = "none";
     document.querySelector("#batteryIconCharging").style.display = "block";
-    if (previousBatteryState == "charging") {
-      batteryIn()
-    }
+    batteryIn();
     previousBatteryState = "charging";
   }
 }
