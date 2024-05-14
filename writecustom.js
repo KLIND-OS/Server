0c079a8d7dd1a1deb61e75aca5e67214073fe2d5
JZITNIK-github <email@jzitnik.is-a.dev>
2024-05-14 21:27:26 +0200

    Minor changes

--- writecustom.js
+++
@@ -1,3 +0,0 @@
-function writecustomapp(icon, name, code) {
-  document.querySelector("#customasdappsadasdasdsad").innerHTML = document.querySelector("#customasdappsadasdasdsad").innerHTML + "<img onclick=\" writeloadapp('" + name + "', '" + code + "')\" alt=\"" + name + "\" src=\"" + icon + "\">";
-}
