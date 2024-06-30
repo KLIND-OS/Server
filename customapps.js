ba3ab2294b8c99d0ad2585161afb73e238646500
JZITNIK-github <email@jzitnik.dev>
2024-06-30 17:26:42 +0200

    chore: Force async install

--- customapps.js
+++ customapps.js
@@ -162,18 +162,11 @@ var CustomApp = {
 
     await LowLevelApi.filesystem.fsExtra.rm(outputPath, { recursive: true });
 
-    if (install.trim().startsWith('"use async"')) {
-      window.installFinished = () => {
-        all.push([name, script, icon]);
-        localStorage.setItem("customapps", JSON.stringify(all));
-        window.location.reload();
-      };
-      eval(install);
-    } else {
-      eval(install);
+    window.installFinished = () => {
       all.push([name, script, icon]);
       localStorage.setItem("customapps", JSON.stringify(all));
       window.location.reload();
-    }
+    };
+    eval(install);
   },
 };
