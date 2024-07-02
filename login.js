2a980087d4fb614fe37c9f8ff9a1dac2419a3dde
JZITNIK-github <email@jzitnik.dev>
2024-07-02 12:49:22 +0200

    feat: Some more refactoring

--- login.js
+++ login.js
@@ -23,6 +23,7 @@ class Login {
       document.querySelector(".login").style.opacity = "1";
     }, 10);
   }
+
   static load() {
     setInterval(() => {
       var d = new Date();
@@ -156,4 +157,23 @@ class Login {
       document.querySelector("#facerecif").src = "about:blank";
     }, 500);
   }
+
+  static async loadFaceButton() {
+    if (
+      localStorage.getItem("fa1c2e") == null ||
+      localStorage.getItem("fa1c2e") == "" ||
+      (await parent.mainFileManager.fileExists(
+        JSON.parse(localStorage.getItem("fa1c2e"))[0],
+      )) == false ||
+      (await parent.mainFileManager.fileExists(
+        JSON.parse(localStorage.getItem("fa1c2e"))[1],
+      )) == false
+    ) {
+      document.querySelector("#sdsdaasd").style.display = "none";
+    } else {
+      document.querySelector("#sdsdaasd").style.display = "block";
+    }
+  }
 }
+
+window.Login = Login;
